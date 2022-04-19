d7b2266c528e5c4e5dc27269febd8b7eb316d608
Aditya Tandon <adityatandon16101@gmail.com>
2022-04-19 19:27:55 +0530

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->mediumText('uuid')->nullable();
+            $table->mediumText('photo')->nullable();
+            $table->mediumText('referral_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('status');
+            $table->boolean('banned');
             $table->timestamps();
         });
     }
